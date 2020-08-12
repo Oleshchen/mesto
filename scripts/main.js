e2d9948aa764dadbf35fd36e5a6d03c066ec2b95
Anton Oleshchenko <oleshchen@yandex.ru>
2020-08-12 23:41:39 +0300

    3 rev

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,25 @@
 let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__info-button');
 let popupCloseButton = popup.querySelector('.popup__close-button');
+let nameInput = popup.querySelector('.popup__input_name');
+let jobInput = popup.querySelector('.popup__input_profession');
+let infoName = document.querySelector('.profile__info-name');
+let profession = document.querySelector('.profile__profession');
 
 let popupTogle = function() {
   popup.classList.toggle ('popup_visibility');
+  jobInput.value = '';
+  nameInput.value = '';
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
-    
-  let nameInput = popup.querySelector('.popup__form_name');
-  let jobInput = popup.querySelector('.popup__form_profession');
 
   let nameInputValue = nameInput.value;
   let jobInputValue = jobInput.value;
-
-  let infoName = document.querySelector('.profile__info-name');
-  let profession = document.querySelector('.profile__profession');
-
   infoName.textContent = nameInputValue;
   profession.textContent = jobInputValue;
+
   popup.classList.remove('popup_visibility');
 }
 popupOpenButton.addEventListener('click', popupTogle);
